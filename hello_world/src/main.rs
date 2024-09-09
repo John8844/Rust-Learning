e58a94d16d0095ab69a45942825349ca648defb5
Johnson <johnson@kalakendradao.org>
2024-09-09 10:52:24 +0530

    Implemented hello world and guessing game program

---
+++ hello_world/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+     let guess = 10;
+     println!("Guess {guess}");
+
+     let mut stri = String::new();
+     println!("Guess {stri}");
+     stri = (&"john").to_string();
+     println!("Guess {stri}");
+}
